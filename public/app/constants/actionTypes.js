294f0acc4fd253f60244916530cee5da63615399
move132 <move11@126.com>
2016-06-01 16:38:12 +0800

    init

---
+++ public/app/constants/actionTypes.js
@@ -0,0 +1,21 @@
+import keyMirror from 'fbjs/lib/keyMirror'
+
+ 
+/*var COLORS = keyMirror({blue: null, red: null});
+var Ts=keyMirror({
+    ADD_ITEM: null,
+    DELETE_ITEM: null,
+    DELETE_ALL: null,
+    FILTER_ITEM: null
+});
+var myColor = COLORS.blue;
+var isColorValid = !!COLORS[myColor];
+console.log(Ts); */
+
+export default keyMirror({ 
+    ADD_ITEM: null,
+    DELETE_ITEM: null,
+    DELETE_ALL: null,
+    FILTER_ITEM: null
+})
+   
---
+++ public/app/reducers/items.js
@@ -0,0 +1,20 @@
+import Immutable from 'immutable'
+import { ADD_ITEM, DELETE_ITEM, DELETE_ALL } from '../constants/actionTypes'
+
+const initialItems = Immutable.List([{name:'jone',num:1},{name:'object',num:2},{name:'mocha',num:3}])
+
+console.log(initialItems,initialItems.get(0));
+
+export default function items(state = initialItems, action) {
+    switch(action.type) {
+        case ADD_ITEM: 
+            var items={name:'jone_'+state.get(-1).num + 1,num:( state.size !== 0 ? state.get(-1).num + 1 : 1 )};
+            return state.push(items);
+        case DELETE_ITEM: 
+            return state.delete(state.indexOf(action.item))
+        case DELETE_ALL:
+            return state.clear()
+        default:
+            return state
+    }
+}
